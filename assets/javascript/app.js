969d68cdd033547859f795dd01b61b9af1ab22fb
deep-research <deep-research>
2017-10-21 10:04:09 -0500

    Form reset

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 $(document).ready( function() {
 
-	// Initialize FireBase
+	// Initialize Firebase
 	var config = {
 	    apiKey: "AIzaSyB8hx1F9ar1GsikbMCFw1lK7k61lnAvxjQ",
 	    authDomain: "firstdatabase-83848.firebaseapp.com",
@@ -11,7 +11,7 @@ $(document).ready( function() {
 	firebase.initializeApp(config);;
 	var database = firebase.database();
 
-	// New Train Information
+	// Empty variable for data transfer
 	var trainName;
 	var trainDestination;
 	var startTime;
@@ -25,11 +25,17 @@ $(document).ready( function() {
 	// Submit Button
 	$("#submit-btn").on("click", function(event) {
 		event.preventDefault();
+
+		// Extract data from the web form
 		trainName = $("#train-name").val().trim();
 		trainDestination = $("#train-destination").val().trim();
 		startTime = $("#start-time").val().trim();
 		trainFrequency = $("#train-frequency").val().trim();
 
+		// Reset form after submission
+		$("#train-form").trigger("reset");
+
+		// Send Data to Firebase
 		database.ref().push({
 			name: trainName,
 			destination: trainDestination,
@@ -40,25 +46,23 @@ $(document).ready( function() {
 
 	// Data Retrieval
 	database.ref().on("child_added", function(childSnapshot) {
+
+		// Get the latest train information after any updates
 	    var childData = childSnapshot.val();
-	    var trainName = childData.name
-	    var trainDestination = childData.destination
-	    var startTime = childData.time
-	    var trainFrequency = childData.frequency
+	    var trainName = childData.name;
+	    var trainDestination = childData.destination;
+	    var startTime = childData.time;
+	    var trainFrequency = childData.frequency;
 
+	    // Moment.JS time calculations
 		var startTimeConverted = moment(startTime, "HH:mm").subtract(1, "years");
-
     	var currentTime = moment();
-
 	    var diffTime = moment().diff(moment(startTimeConverted), "minutes");
-
 	    var tRemainder = diffTime % trainFrequency;
-
 	    var tMinutesTillTrain = trainFrequency - tRemainder;
-
 	    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
 
-	    // Display in the Table
+	    // Display latest train inforamtion in the web table
 	    $("#train-rows").append("<tr>" +
 	      						"<td>" + trainName + "</td>" +
 	      						"<td>" + trainDestination + "</td>" +
